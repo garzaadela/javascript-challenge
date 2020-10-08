d7c4cb5d64aea4bfc9c6a2603149dd1bc28d92af
Adela Garza <a.garza045@gmail.com>
2020-10-08 18:27:58 -0500

    updated hw

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
-var tableData = data;
+let tableData = data;
+let table = d3.select("#ufo-table")
+function constructTable(input){
+     // First, clear out any existing data
+    table.html("");
+    input.forEach(x => {
+        // creates a new html tag that will be appended to the table
+        let row = table.append("tr")
+        console.log(Object.values(x))
+        Object.values(x).forEach((val) => {
+            let cell = row.append("td");
+              cell.text(val);
+            })
+        console.log(x['city'])
+    })
+}
+constructTable(tableData)
 
-// YOUR CODE HERE!
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+function handleClick() {
+    let input = d3.select("#datetime").property("value")
+    console.log(input)
+    let result = tableData.filter(word => word["datetime"] == input);
+    constructTable(result)
+}
